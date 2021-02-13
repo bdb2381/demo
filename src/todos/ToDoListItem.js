6b9fafa1f8b740ea2fa9b868de561910d9210d88
bdb2381 <brad.beggs@gmail.com>
2021-02-13 10:49:57 -0800

    create ToDoListItem

---
+++ src/todos/ToDoListItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ToDoListItem = ({ todo }) => (
+  <div className="todo-item-container">
+    <h3>{todo.text}</h3>
+    <div className="buttons-container">
+      <button className="completed-button">
+        Mark Completed
+      </button>
+      <button className="remove-button">
+        Remove
+      </button>
+    </div>
+  </div>
+)
+
+export default ToDoListItem
