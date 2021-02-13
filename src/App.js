c7e9bd13861bd963c213928a946237146c6d7928
bdb2381 <brad.beggs@gmail.com>
2021-02-13 10:14:25 -0800

    finished building base app files and package installs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import {hot} from 'react-hot-loader'
 import './App.css'
 
 const App = () => (
   <div className="App">
-    <h1>Welcome to Demo, App to Play w/ React</h1>
+    <h1>Welcome to Demo, an App for Playing w/ React</h1>
   </div>
 )
 
-export default App
+export default hot(module)(App)
