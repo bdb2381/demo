00aff1dd0d3293819f22ca3eae1d32c70dfe561a
bdb2381 <brad.beggs@gmail.com>
2021-02-18 10:01:01 -0800

    installed redux-persist package and setup store.js for persist-redux

--- src/store.js
+++ src/store.js
@@ -1,10 +1,19 @@
 import { createStore, combineReducers } from "redux";
-import {toDos} from './todos/reducers'
-
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
+import { toDos } from './todos/reducers'
 
 const reducers = {
   toDos,
 };
-const rootReducer = combineReducers(reducers);
+const persistConfig = {
+  key: 'root',
+  storage,
+  stateReconciler: autoMergeLevel2
+}
 
-export const configureStore = () => createStore(rootReducer)
+const rootReducer = combineReducers(reducers);
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const configureStore = () => createStore(persistedReducer)
