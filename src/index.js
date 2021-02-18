1b8bf77a30d3f5d1df6eb0e7af8884e9ae2402a8
bdb2381 <brad.beggs@gmail.com>
2021-02-18 10:40:55 -0800

    installed and setup redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import {configureStore} from'./store'
+import { configureStore } from'./store'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/lib/integration/react'
 import App from "./App.js";
 
+const store = configureStore()
+const persistor = persistStore(store)
 
 ReactDOM.render(
-  <Provider store={configureStore()}>
-    <App />
+  <Provider store={ store }>
+    <PersistGate
+      loading={<div>Loading...</div>} // show until Persist loads the application data
+      persistor={ persistor }
+    >
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers } from "redux";
 import { persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { toDos } from './todos/reducers'
 
 const reducers = {
@@ -9,11 +10,13 @@ const reducers = {
 };
 const persistConfig = {
   key: 'root',
-  storage,
-  stateReconciler: autoMergeLevel2
+  storage,  // defaults to localStorage
+  stateReconciler: autoMergeLevel2 // how deep to reconcile storage
 }
 
 const rootReducer = combineReducers(reducers);
 const persistedReducer = persistReducer(persistConfig, rootReducer)
+const store = createStore(persistedReducer, composeWithDevTools())
 
-export const configureStore = () => createStore(persistedReducer)
+
+export const configureStore = () => store
