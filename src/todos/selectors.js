621af352cb6880f8540f423b6707c71f7fe73a7d
bdb2381 <brad.beggs@gmail.com>
2021-03-04 15:59:57 -0800

    installed reselect package

--- src/todos/selectors.js
+++ src/todos/selectors.js
@@ -1,2 +1,8 @@
-export const getToDos = (state) =>  state.toDos.data
+import {createSelector} from 'reselect'
+
+export const getToDos = (state) => state.toDos.data
 export const getToDosLoading = (state) => state.toDos.isLoading
+export const getIncompleteToDos = (state) => createSelector(
+  getToDos,
+  (toDos) => toDos.filter(toDo => !toDo.isCompleted),
+)
