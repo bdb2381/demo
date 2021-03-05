e958b32143a91b88f9b2e860cc135d4ef604cfee
bdb2381 <brad.beggs@gmail.com>
2021-03-04 16:08:41 -0800

    getIncompleteToDos function written

--- src/todos/selectors.js
+++ src/todos/selectors.js
@@ -3,6 +3,11 @@ import {createSelector} from 'reselect'
 export const getToDos = (state) => state.toDos.data
 export const getToDosLoading = (state) => state.toDos.isLoading
 export const getIncompleteToDos = (state) => createSelector(
-  getToDos,
-  (toDos) => toDos.filter(toDo => !toDo.isCompleted),
+  getToDos, // first call
+  getToDosLoading, // second call
+  //return values from above two func are the agruments for the arrow function
+  // createSelector takes a variable number of agruments and the last call in the argument list returns the combined result
+  (toDos, isLoading) => isLoading
+    ? []
+  : toDos.filter(toDo => !toDo.isCompleted),
 )
